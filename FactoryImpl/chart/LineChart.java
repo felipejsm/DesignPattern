01b0a03ee889587b3ef2e4affbd5df90c80c04df
felipejsm <felipejsm41@gmail.com>
2017-07-25 21:29:52 -0300

    Removing null verification

--- FactoryImpl/chart/LineChart.java
+++ FactoryImpl/chart/LineChart.java
@@ -1,11 +1,8 @@
 public class LineChart extends Chart {
     public IFactory factory;
     public LineChart(IFactory factory) {
-        if(factory != null) {
-            this.factory = factory;
-            createChart();
-        }
-
+           this.factory = factory;
+           createChart();
     }
     private void createChart() {
        legend = this.factory.createLegend();
