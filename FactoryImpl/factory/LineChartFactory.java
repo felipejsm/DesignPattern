a46bee9017bf5399791118fb85a84c010a6a0496
felipejsm <felipejsm41@gmail.com>
2017-07-25 21:34:17 -0300

    Just methods declarations

--- FactoryImpl/factory/LineChartFactory.java
+++ FactoryImpl/factory/LineChartFactory.java
@@ -4,4 +4,13 @@ public class LineChartFactory implements IFactory{
     public Legend createLegend() {
 
     }
-}
+    public Title createTitle() {
+
+    }
+    public List<Series> createSeries() {
+
+    }
+    public List<Xaxis> createXaxis() {
+        
+    }
+ }
