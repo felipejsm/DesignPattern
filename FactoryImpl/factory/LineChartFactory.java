0ca2af2d90e7451fa435ce34a0a2812da7cb49d4
felipejsm <felipejsm41@gmail.com>
2017-07-31 20:04:19 -0300

    Implmenting Line Factory I

--- FactoryImpl/factory/LineChartFactory.java
+++ FactoryImpl/factory/LineChartFactory.java
@@ -2,12 +2,21 @@ import chart.factory.IFactory;
 public class LineChartFactory implements IFactory{
     
     public Legend createLegend() {
-
+        Legend legend = new Legend();
+        String [] data = {"Coke","Diet Coke"}; 
+        legend.setData(data);
+        return data;
     }
     public Title createTitle() {
-
+        Title title = new Title();
+        String text = "Preferences";
+        title.setText(text);
+        return title;
     }
     public List<Series> createSeries() {
+        List<Series> series = new ArrayList<>();
+        //#1 serie
+        Series serieCoke = new Series();
 
     }
     public List<Xaxis> createXaxis() {
